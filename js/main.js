eeccaa56139dc50305ea1d253f9135dc0c610183
artur.emelyanov99 <artur.emelyanov99@gmail.com>
2022-10-19 13:14:28 +0300

    Fixed bugs

--- js/main.js
+++ js/main.js
@@ -1,22 +1,25 @@
+const body = document.body;
 const openPopup = document.getElementById('open-popup');
 const closePopup = document.getElementById('close-popup');
 const closePopupMenuLink = document.getElementById('close-popup-menu-link')
 const popup = document.getElementById('popup');
 const mybutton = document.getElementById("toTop");
 
-
 // Popup
 openPopup.addEventListener('click', function(e){
     e.preventDefault();
     popup.classList.add('active');
+    body.classList.add('disabled');
 })
 
 closePopup.addEventListener('click', () => {
     popup.classList.remove('active');
+    body.classList.remove('disabled');
 })
 
 closePopupMenuLink.addEventListener('click', () => {
     popup.classList.remove('active');
+    body.classList.remove('disabled');
 })
 
 
